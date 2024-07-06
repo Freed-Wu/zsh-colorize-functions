a513e8f63d7aabaa757b71b3b9a1eed123d4b547
Wu, Zhenyu <wuzhenyu@ustc.edu>
2024-07-06 22:02:04 +0800

    :bug: Colorize `declare -f` and `declare`

--- zsh-colorize-functions.plugin.zsh
+++ zsh-colorize-functions.plugin.zsh
@@ -8,7 +8,8 @@ function has_cmd() {
 if has_cmd bat; then
   if [ -n "$ZSH_VERSION" ]; then
     function functions() {
-      local arg is_colorize=1
+      local arg
+      local -i is_colorize=1
       # https://github.com/Freed-Wu/fzf-tab-source/issues/12
       for arg; do
         [[ $arg == -? && $arg != -- ]] && is_colorize=0 && break
@@ -20,13 +21,31 @@ if has_cmd bat; then
       fi
     }
     function declare() {
-      builtin declare "$@" | bat -pplzsh
+      local arg
+      local -i is_colorize=1
+      for arg; do
+        [[ $arg != -f ]] && is_colorize=0 && break
+      done
+      if ((is_colorize)) ; then
+        builtin declare "$@" | bat -pplzsh
+      else
+        builtin declare "$@"
+      fi
     }
     alias declare=\declare
     unfunction has_cmd
   else
     function declare() {
-      builtin declare "$@" | bat -pplbash
+      local arg
+      local -i is_colorize=1
+      for arg; do
+        [[ $arg != -f ]] && is_colorize=0 && break
+      done
+      if ((is_colorize)) ; then
+        builtin declare "$@" | bat -pplbash
+      else
+        builtin declare "$@"
+      fi
     }
     unset has_cmd
   fi
